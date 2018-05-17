0454180505ff668aa787a9de3de782828604a3f1
blackjws <blackxin55+@gmail.com>
2018-05-17 17:42:00 +0800

    新增读取摄像头模块

---
+++ src/utils/video_stream.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+# coding: utf-8 
+""" 
+   @author: kenwood
+   @time: 18-5-17 下午12:47  
+"""
+import cv2
+class Camera():
+    def __init__(self,src):
+        self.video = cv2.VideoCapture(src)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        _,frame = self.video.read()
+        return frame
+
+
