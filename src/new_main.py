72b716c2b59f56868514e242d6c8126a15545303
blackjws <blackxin55+@gmail.com>
2018-05-21 16:27:20 +0800

    todo

--- src/new_main.py
+++ src/new_main.py
@@ -10,16 +10,25 @@ from src.Config import Config
 from src.utils import video_stream,Constant
 #配置对象
 config = Config()
-
-
-#读取摄像头帧
-frame = video_stream.Camera(config.get(Constant.VIDEO_SOURCE))
-#检测人脸
 detecter = MtcnnDetetion(config)
-locations, landmarks = detecter.detect(frame)
+
+#Todo 加载人脸特征库接口
+# feature = f (config)
+feature = pickle.load('')
+
+while True:
+    #读取摄像头帧
+    frame = video_stream.Camera(config.get(Constant.VIDEO_SOURCE))
+    #检测人脸
+
+    #人脸位置，人脸特征点
+    locations, landmarks = detecter.detect(frame)
+    #Todo 人脸特征抽取
+
+    #Todo  人脸匹配，在人脸特征库中进行匹配．
 
 
 
-#Todo 注释
-(frame, face_names, now_time, image) = old_faceRecognition.recognition(
-    frame, EncodingCache, known_face_dataset)
+    #Todo 注释
+    (frame, face_names, now_time, image) = old_faceRecognition.recognition(
+        frame, EncodingCache, known_face_dataset)
