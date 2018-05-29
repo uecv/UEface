b92841358d5a3eecb2dcb15e5014b014aa3a4a86
blackjws <blackxin55+@gmail.com>
2018-05-29 15:32:53 +0800

    增加定时调度，多线程

--- tnado_app.py
+++ tnado_app.py
@@ -1,35 +1,81 @@
-#!/usr/bin/env python 
-# coding: utf-8 
-""" 
+#!/usr/bin/env python
+# coding: utf-8
+"""
    @author: kenwood
-   @time: 18-5-29 上午9:34  
+   @time: 18-5-29 上午9:34
 """
 import tornado.ioloop
+from tornado.ioloop import  IOLoop
 import tornado.web
+import tornado.httpserver
 import tornado.websocket
 from PIL import Image
 from io import BytesIO
 import base64
+import time
+from src.utils.redis_queue import RedisQueue
+#队列
+queue = RedisQueue('rq', host='192.168.0.245', port=6379, db=0)
 
-cl = []
 class SocketHandler(tornado.websocket.WebSocketHandler):
+    def simple_init(self):
+        self.last = time.time()
+        self.stop = False
+
+    def open(self):
+        self.simple_init()
+        print("New client connected")
+        self.write_message("You are connected")
+        #定时调度器
+        self.loop = tornado.ioloop.PeriodicCallback(
+            self.check_ten_seconds, 1000, io_loop=tornado.ioloop.IOLoop.instance())
+        self.loop.start()
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+        self.last = time.time()
+
+    def on_close(self):
+        print("Client disconnected")
+        self.loop.stop()
+
     def check_origin(self, origin):
         return True
 
-    def open(self):
-        if self not in cl:
-            cl.append(self)
+    def check_ten_seconds(self):
+        print("Just checking")
 
-    def on_message(self,message):
-        print ('test')
-        self.write_message(u"You said: ")
+        #people_num
+        msg = {
+            "type": "GET_COUNT",
+            "NUMS": "50"
+        }
+        #recognition result
+        data = queue.get_nowait().decode('utf-8')
+        print(eval(data)['name'])
+        raw_image = queue.get_value(eval(data)['name']).decode('utf-8')
+        result = eval(data)
+        result_dict = {'ts': result['ts'],
+                       'name': result['name'],
+                       'image': result['image'],
+                       'raw_image': raw_image,
+                       'similarity': result['similarity'],
+                       'type': "GET_RECO_RESULT"}
 
-    def on_close(self):
-        if self in cl:
-            cl.remove(self)
+
+        if (time.time() - self.last > 10):
+            self.write_message("You sleeping mate?")
+            self.write_message(msg)
+            self.write_message(result_dict)
+            self.last = time.time()
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
         img = Image.open('/home/kenwood/图片/cat.jpeg', 'r')
         buffered = BytesIO()
         img.save(buffered, format="JPEG")
@@ -49,15 +95,21 @@ class MainHandler(tornado.web.RequestHandler):
                     "url": "http://192.168.0.245:8888/live/livestream.flv",
                     "x": 70,
                     "y": 80,
-                }]
+            }]
         }
         self.write(cam_dict)
-
-
-if __name__ == "__main__":
-    application = tornado.web.Application([
+def make_app():
+    return tornado.web.Application([
         (r"/", MainHandler),
         (r'/ws', SocketHandler),
     ])
-    application.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+
+def main():
+    app = make_app()
+    server = tornado.httpserver.HTTPServer(app)
+    server.bind(8888)
+    server.start(0)  # forks one process per cpu
+    IOLoop.current().start()
+
+if __name__ == '__main__':
+    main()
