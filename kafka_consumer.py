282af4bc52e0ab46f644ad71899237d70c3e4307
blackjws <blackxin55+@gmail.com>
2018-06-06 11:38:09 +0800

    add kafka_producer

---
+++ kafka_consumer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# coding: utf-8 
+""" 
+   @author: kenwood
+   @time: 18-6-6 上午10:38  
+"""
---
+++ pyspark_main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# coding: utf-8 
+""" 
+   @author: kenwood
+   @time: 18-6-5 下午3:32  
+"""
