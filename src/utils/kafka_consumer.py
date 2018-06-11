b9c0b0bbc7d200801fb1ad59c54d1bd7ec5d8faf
blackjws <blackxin55+@gmail.com>
2018-06-11 17:20:43 +0800

    kafka new

--- src/utils/kafka_consumer.py
+++ src/utils/kafka_consumer.py
@@ -4,13 +4,18 @@
    @author: kenwood
    @time: 18-6-6 上午10:38  
 """
-from pykafka import KafkaClient
-
-hosts = '192.168.0.251:9092'
-client = KafkaClient(hosts=hosts)
-# 消费者
-topic = client.topics[b'frame']
-consumer = topic.get_simple_consumer()
+from kafka import KafkaConsumer
+from PIL import Image
+import io
+import numpy as np
+consumer = KafkaConsumer('frame',
+                         bootstrap_servers=['192.168.0.245:9092'],api_version = (0, 10))
 for message in consumer:
-    if message is not None:
-        print (message.offset, message.value)
+    # print ("%s:%d:%d: key=%s value =%s" % (message.topic, message.partition,
+    #                                       message.offset, message.key,message.value))
+    # new_frame = (np.fromstring(message.value, dtype=np.uint8))
+    # print (new_frame)
+    image_data = message.value  # byte values of the image
+    image = np.asarray(Image.open(io.BytesIO(image_data)))
+    print(image)
+
--- src/utils/kafka_producer.py
+++ src/utils/kafka_producer.py
@@ -4,45 +4,53 @@
    @author: kenwood
    @time: 18-6-6 上午10:24  
 """
-from pykafka import KafkaClient,Broker
-import time
-import json
-import uuid
-import random
+# from pykafka import KafkaClient,Broker
+from kafka import KafkaProducer,SimpleProducer,SimpleClient
+from kafka.errors import KafkaError
+import numpy as np
 import threading
 import cv2
-#创建kafka实例
-hosts = '192.168.0.251:9092'
-client = KafkaClient(hosts=hosts)
-
-print(client.topics)
-
-# 创建kafka producer句柄
-topic = client.topics[b'frame']
+import time
+import io
 
 src = "rtsp://admin:qwe123456@192.168.1.202:554/cam/realmonitor?channel=1&subtype=0"
 video_capture = cv2.VideoCapture(src)
+#创建kafka实例
+producer = KafkaProducer(bootstrap_servers='192.168.0.245:9092',max_request_size=4000000,api_version = (0, 10))
+
+
+
+def video_emitter(video):
+    # Open the video
+    video = cv2.VideoCapture(video)
+    print(' emitting.....')
+
+    # read the file
+    while (video.isOpened):
+        # read the image in each frame
+        success, image = video.read()
+        # check if the file has read to the end
+        if not success:
+            break
+        # convert the image png
+        ret, jpeg = cv2.imencode('.png', image)
+        # Convert the image to bytes and send to kafka
+        producer.send('frame', jpeg.tobytes())
+        # To reduce CPU usage create sleep time of 0.2sec
+        time.sleep(0.2)
+    # clear the capture
+    video.release()
+    print('done emitting')
+
+if __name__ == '__main__':
+    video_emitter(src)
+
 
 # work
-def work():
-    with topic.get_producer(max_queued_messages=500000,max_request_size=4000012) as producer:
-        while True:
-            video_capture.set(cv2.CAP_PROP_FPS,5)
-            _, frame = video_capture.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # msg = json.dumps({
-            #     "id": str(uuid.uuid4()).replace('-', ''),
-            #     "type": random.randint(1, 5),
-            #     "profit": random.randint(13, 100)}).encode('utf-8')
-            producer.produce(frame.tobytes())
-            time.sleep(60)
-
 # 多线程执行
 # thread_list = [threading.Thread(target=work) for i in range(10)]
 # for thread in thread_list:
 #     thread.setDaemon(True)
-#     thread.start()
-if __name__ == '__main__':
-    work()
+
 
 
