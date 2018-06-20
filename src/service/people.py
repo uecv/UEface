f7ad137370e264ebcb35faffb2d9b7a2566eded6
weiy <weiyu@useease.com>
2018-06-20 15:18:47 +0800

    people 类操作的时候，key重复问题修改

--- src/service/people.py
+++ src/service/people.py
@@ -10,13 +10,17 @@ import datetime
 from src.storage import db
 session = db.Session()
 
+
+def generate_uuid():
+    return str(uuid.uuid1())
+
 class People(db.Base):
     __tablename__ = 'people'
     __table_args__ = {'extend_existing':True}
     """
     实体类,对应数据库中people表
     """
-    id = Column(String(36),default=uuid.uuid1(), primary_key=True)
+    id = Column(String(36), primary_key=True)
     name = Column(String(20),nullable=False)
     company_id = Column(String(20),nullable=False)
     worker_id = Column(String(20),nullable=False)
@@ -33,9 +37,9 @@ def insert_people(people):
     :param people:
     :return:
     """
+    people.id = generate_uuid()
     session.add(people)
     session.commit()
-
 
 
 def get_peoples():
@@ -55,9 +59,13 @@ def get_people(uuid):
 
 
 if __name__ == '__main__':
-    p = People(name = 'qwe',worker_id='sdsfsdf',company_id="ue",image_path = 'dsfsd')
-    session.add(p)
-    session.commit()
+
+    for i in range(0,3):
+        p = People(name = str(i),worker_id='sdsfsdf',company_id="ue",image_path = 'dsfsd')
+        insert_people(p)
+
+    # 查询所有数据
     print(get_peoples())
+    # 查询个别数据
     our_user = session.query(People).filter_by(name='qwe').first()
     print(our_user)
