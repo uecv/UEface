f690c62bd9183fbc546463af9e503127156e4daf
kenwoodjw <blackxin55+@gmail.com>
2018-08-21 16:09:41 +0800

    Update faceNetFeatureLib.py
    merge sigma

--- src/library/faceNetLib/faceNetFeatureLib.py
+++ src/library/faceNetLib/faceNetFeatureLib.py
@@ -8,8 +8,10 @@ class  faceNetLib:
 
 
     def getlib(self):
-        f = open(self._path, 'r')
-        known_face_dataset = json.loads(f.read())  # 人脸特征库
-        f.close()
+        known_face_dataset = featuresDB.getFeature()
+
+#         f = open(self._path, 'r')
+#         known_face_dataset = json.loads(f.read())  # 人脸特征库
+#         f.close()
 
         return known_face_dataset
